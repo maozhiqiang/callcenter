0de05299cdf669164ea9997df1456a74e4d875b3
whc <wuhaichao@aicyber.com>
2017-11-15 15:03:35 +0800

    +++

---
+++ brings/Config.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+__author__ = 'Arvin.Wu'
+
+#====================POSGRESQL===================
+
+DATABASE_HOST = "10.9.90.133" #要链接的服务器的IP地址，local-IP
+#DATABASE_HOST = "192.168.0.183" #要链接的服务器的IP地址，Server-IP
+DATABASE_PORT = 5432 #postgresql端口号，安装postgresql时设置的，一般默认是5432
+DATABASE_NAME = "call" #数据库名称
+DATABASE_USERNAME = "root" #用户名，这里的用户名也是在安装postgresql时设置的，一般默认postgres
+DATABASE_PASSWORD = "z8asuidnaicyber" #链接数据库服务器的密码，安装时设置，安装完毕后可以修改的
+#==========
+#postgresql     username:call
+#               user :root
+#               passsword: z8asuidnaicyber
+#
