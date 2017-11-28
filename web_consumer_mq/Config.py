29792f97ae93a630dd62c57d7013c9bc346ff50d
whc <wuhaichao@aicyber.com>
2017-11-28 17:50:45 +0800

    +++

--- web_consumer_mq/Config.py
+++ web_consumer_mq/Config.py
@@ -24,14 +24,13 @@ key = 'The nature of the polymer is currently a trade secret'
 #=============DB config ===================================
 
 DATABASE_HOST = "10.9.111.12" #要链接的服务器的IP地址，local-IP
-#DATABASE_HOST = "192.168.0.183" #要链接的服务器的IP地址，Server-IP
 DATABASE_PORT = 5432 #postgresql端口号，安装postgresql时设置的，一般默认是5432
-DATABASE_NAME = "call" #数据库名称
-DATABASE_USERNAME = "root" #用户名，这里的用户名也是在安装postgresql时设置的，一般默认postgres
-DATABASE_PASSWORD = "z8asuidnaicyber" #链接数据库服务器的密码，安装时设置，安装完毕后可以修改的
+DATABASE_NAME = "postgres" #数据库名称
+DATABASE_USERNAME = "postgres" #用户名，这里的用户名也是在安装postgresql时设置的，一般默认postgres
+DATABASE_PASSWORD = "z8asuidn" #链接数据库服务器的密码，安装时设置，安装完毕后可以修改的
 
 #==================RabbtiMq ===============================
-MQ_URL = '10.9.169.85'
+MQ_URL = '127.0.0.1'
 
 
 
--- web_consumer_mq/rabbtiMQ_consume.py
+++ web_consumer_mq/rabbtiMQ_consume.py
@@ -15,45 +15,45 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
     conf.MQ_URL,5672,'/',credentials))
 channel = connection.channel()
 
-# You may ask why we declare the queue again ‒ we have already declared it in our previous code.
-# We could avoid that if we were sure that the queue already exists. For example if send.py program
-# was run before. But we're not yet sure which program to run first. In such cases it's a good
-# practice to repeat declaring the queue in both programs.
 channel.queue_declare(queue='durable',durable=True)
-
 
 def callback(ch, method, properties, body):
     logger.info(" [x] Received %r\n\n" % body)
     dict = json.loads(body)
-    print '**********',dict
+    # print '**********',dict
     if dict['mark'] == 'insert':
-        print  '-------------', dict['who'], dict['text'], dict['record_fpath'], dict['create_at'], dict['call_id'], \
-        dict['jsonStr']
         sql = 'INSERT INTO fs_call_replay(who, text, record_fpath, create_at, call_id,resp_param)VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\',\'{5}\')'.format(
             dict['who'], dict['text'], dict['record_fpath'], dict['create_at'],dict['call_id'] , dict['jsonStr'])
-
+        run_insert_sql(sql)
+        logger.info('run_insert_sql.....%s' % sql)
     else:
-        print '................',dict['record_fpath'],dict['channal_uuid']
+        # print '................',dict['record_fpath'],dict['channal_uuid']
         sql = "update fs_call set full_record_fpath ='{0}' where channal_uuid ='{1}'".format(dict['record_fpath'],dict['channal_uuid'] )
-    logger.info('runsql.....%s'%sql)
-    runsql(sql)
+        run_update_sql(sql)
+        logger.info('run_update_sql.....%s' % sql)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def runsql(sql):
+def run_insert_sql(sql):
     try:
         conn = db_pool.getConn()
         cursor = conn.cursor()
         count = cursor.execute(sql, )
-        logger.info("ID of last record is %d"%int(cursor.lastrowid))  # 最后插入行的主键ID
         conn.commit()
         db_pool.close(cursor, conn)
     except Exception as e:
         logger.error('runsql exception error %s '%e)
 
-channel.basic_consume(callback,
-                      queue='durable',
-                      #no_ack=True
-                      )
+def run_update_sql(sql):
+    try:
+        conn = db_pool.getConn()
+        cursor = conn.cursor()
+        count = cursor.execute(sql, )
+        conn.commit()
+        db_pool.close(cursor, conn)
+    except Exception as e:
+        logger.error('runsql exception error %s '%e)
+
+channel.basic_consume(callback,queue='durable')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
