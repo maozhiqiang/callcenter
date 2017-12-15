791ff889de67a44e411d7f4a82f3b3ea4a91df90
whc <wuhaichao@aicyber.com>
2017-12-15 15:55:37 +0800

    update app

--- fs_handler_callback/Config.py
+++ fs_handler_callback/Config.py
@@ -2,35 +2,17 @@
 __author__ = 'Arvin.Wu'
 
 #====================POSGRESQL===================
-
 DATABASE_HOST = "118.190.166.165" #要链接的服务器的IP地址，local-IP
-#DATABASE_HOST = "192.168.0.183" #要链接的服务器的IP地址，Server-IP
 DATABASE_PORT = 5432 #postgresql端口号，安装postgresql时设置的，一般默认是5432
 DATABASE_NAME = "call_api" #数据库名称
 DATABASE_USERNAME = "postgres" #用户名，这里的用户名也是在安装postgresql时设置的，一般默认postgres
 DATABASE_PASSWORD = "z8asuidn" #链接数据库服务器的密码，安装时设置，安装完毕后可以修改的
-#==========
-#postgresql     username:call
-#               user :root
-#               passsword: z8asuidnaicyber
-#
-
-#====================QueueManager===============
-queue_ip = '192.168.0.183'
-queue_port = 50000
-queue_authkey = 'aicyberqueue'
-
 #====================ESL  Config==================
-
-ESL_HOST = '118.190.166.134'#121.42.31.97
+ESL_HOST = '39.106.166.177'#121.42.31.97
 ESL_PORT = 8021
 ESL_PWD = 'Aicyber'
 
-#====================扣费 云上==================
-#server_url = 'http://121.42.36.138/api/finance/minute/update/'
-#server_url = 'http://10.9.169.85/api/finance/minute/update/'
-#====================扣费 local==================
-#server_url = 'http://192.168.0.183:8000/api/finance/minute/update/'
+
 
 
 
