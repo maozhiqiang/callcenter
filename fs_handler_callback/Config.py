7781d693d1504c77c86c6d1c0ba17ffffbb78c77
whc <wuhaichao@aicyber.com>
2018-01-23 15:15:30 +0800

    update app

--- fs_handler_callback/Config.py
+++ fs_handler_callback/Config.py
@@ -2,13 +2,13 @@
 __author__ = 'Arvin.Wu'
 
 #====================POSGRESQL===================
-DATABASE_HOST = "rm-2zeeq8wu95hbrl2gmho.pg.rds.aliyuncs.com" #要链接的服务器的IP地址，local-IP
-DATABASE_PORT = 3432 #postgresql端口号，安装postgresql时设置的，一般默认是5432
+DATABASE_HOST = "rm-2zeeq8wu95hbrl2gm.pg.rds.aliyuncs.com" #要链接的服务器的IP地址，local-IP
+DATABASE_PORT = 3433 #postgresql端口号，安装postgresql时设置的，一般默认是5432
 DATABASE_NAME = "call_api_test" #数据库名称
 DATABASE_USERNAME = "postgres" #用户名，这里的用户名也是在安装postgresql时设置的，一般默认postgres
 DATABASE_PASSWORD = "Z8asuidn" #链接数据库服务器的密码，安装时设置，安装完毕后可以修改的
 #====================ESL  Config==================
-ESL_HOST = '39.106.166.177'#121.42.31.97
+ESL_HOST = '127.0.0.1'#121.42.31.97
 ESL_PORT = 8021
 ESL_PWD = 'Aicyber'
 
